447d9463b7eac49da187f37b968525bcf9df98a9
Mark Downey <mark@codenvy.com>
2014-01-13 14:15:02 +0100

    first version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@ define(['marionette','tools/marionette.override'],function(Marionette){
     var application = new Marionette.Application();
 
     application.on('initialize:after', function() {
-        alert('baker is in the house');
+        
     });
 
     return application;
